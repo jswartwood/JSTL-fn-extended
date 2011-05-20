9ab917f06444343bfc8836f9b689b9ef50fa4e13
Jacob Swartwood <jswartwood@gmail.com>
2011-05-20 16:52:20 -0400

    Adding build script.

--- README.md
+++ README.md
@@ -5,6 +5,5 @@ Drop extended.jar from lib into your application lib folder.
 
 ## To build
 
-	javac src/main/java/org/apache/taglibs/extended/*.java
-	jar cvf lib/extended.jar src/main/java/org/apache/taglibs/extended/*.class src/main/resources
+	./build.sh
 
---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm -rf build
+mkdir build
+
+cp -r src/main/java/org build/
+cp -r src/main/resources/META-INF build/
+
+cd build
+
+javac org/apache/taglibs/extended/*.java
+
+jar cvf ../lib/extended.jar org/apache/taglibs/extended/*.class META-INF
