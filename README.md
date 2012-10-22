28ed3fe456777d90cf19c45641374022bb88ad45
Jacob Swartwood <jswartwood@gmail.com>
2012-10-22 15:27:30 -0400

    Adding API documentation; unifying whitespace.

--- README.md
+++ README.md
@@ -1,9 +1,131 @@
-# Extended JSTL expression language functions
+Extended JSTL expression language functions
+===========================================
+The Extended Tag Library is a collection of tags and functions to be used in addition to the Standard tag library.
 
-## To Use
-Drop extended.jar from lib into your application lib folder.
 
-## To build
+To Use
+------
+Copy extended.jar from lib into your application lib folder.
 
-	./build.sh
+
+API
+---
+### Commons Lang StringUtils functions
+- *Capitalize* =>
+  ```java
+  java.lang.String capitalize(java.lang.String)
+  ```
+- *Uncapitalize* =>
+  ```java
+  java.lang.String uncapitalize(java.lang.String)
+  ```
+- *lastIndexOf* =>
+  ```java
+  int lastIndexOf(java.lang.String, java.lang.String)
+  ```
+- *left* =>
+  ```java
+  java.lang.String left(java.lang.String, int)
+  ```
+- *mid* =>
+  ```java
+  java.lang.String mid(java.lang.String, int)
+  ```
+- *right* =>
+  ```java
+  java.lang.String right(java.lang.String, int)
+  ```
+- *repeat* =>
+  ```java
+  java.lang.String repeat(java.lang.String, int)
+  ```
+- *leftPad* =>
+  ```java
+  java.lang.String leftPad(java.lang.String, int, java.lang.String)
+  ```
+- *rightPad* =>
+  ```java
+  java.lang.String rightPad(java.lang.String, int, java.lang.String)
+  ```
+- *center* =>
+  ```java
+  java.lang.String center(java.lang.String, int, java.lang.String)
+  ```
+- *abbreviate* =>
+  ```java
+  java.lang.String abbreviate(java.lang.String, int)
+  ```
+
+### Commons Lang WordUtils functions
+- *capitalizeAllWords* =>
+  ```java
+  java.lang.String capitalize(java.lang.String)
+  ```
+
+### Regex String Replacement
+- *replaceAll* =>
+  ```java
+  java.lang.String replaceAll(java.lang.String, java.lang.String, java.lang.String)
+  ```
+- *replaceFirst* =>
+  ```java
+  java.lang.String replaceFirst(java.lang.String, java.lang.String, java.lang.String)
+  ```
+
+### java.lang.Math functions
+- *ceil* =>
+  ```java
+  double ceil(double)
+  ```
+- *floor* =>
+  ```java
+  double floor(double)
+  ```
+- *round* =>
+  ```java
+  long round(double)
+  ```
+- *sqrt* =>
+  ```java
+  double sqrt(double)
+  ```
+
+### java.lang.System
+- *env* =>
+  ```java
+  java.lang.String getenv(java.lang.String)
+  ```
+- *property* =>
+  ```java
+  java.lang.String property(java.lang.String)
+  ```
+
+### java.net.URLEncoder/URLDecoder
+- *encode* =>
+  ```java
+  java.lang.String encode(java.lang.String, java.lang.String)
+  ```
+- *decode* =>
+  ```java
+  java.lang.String decode(java.lang.String, java.lang.String)
+  ```
+
+### MD5 Hex encoding
+- *md5hex* =>
+  ```java
+  java.lang.String md5hex(java.lang.String)
+  ```
+
+### Weak URL fetch and cache
+- *fetchUrl* =>
+  ```java
+  java.lang.String fetch(java.lang.String)
+  ```
+
+
+To build
+--------
+```
+./build.sh
+```
 
--- src/main/java/org/apache/taglibs/extended/FetchUrl.java
+++ src/main/java/org/apache/taglibs/extended/FetchUrl.java
@@ -6,50 +6,50 @@ import java.util.*;
 
 public class FetchUrl {
 
-    // Define the cache object
-    static class UrlCacheEntry {
-        public String value;
-        public Date date;
+  // Define the cache object
+  static class UrlCacheEntry {
+    public String value;
+    public Date date;
+  }
+
+  // Define the cache as a WeakHasMap of dated content
+  static WeakHashMap<String, UrlCacheEntry> urlCache = new WeakHashMap();
+
+  // This is the function used in the JSTL extended lang
+  // Returns the html response for a given `url` param
+  public static String fetch(String url) throws Exception {
+    // Now's date
+    GregorianCalendar calendar = new GregorianCalendar();
+    Date now = calendar.getTime();
+
+    // First check the cache... does it exist? and is it recent enough?
+    UrlCacheEntry cached = urlCache.get(url);
+    if (cached != null && now.before(cached.date)) {
+       return cached.value;
     }
 
-    // Define the cache as a WeakHasMap of dated content
-    static WeakHashMap<String, UrlCacheEntry> urlCache = new WeakHashMap();
+    // Go fetch the URL
+    URL page = new URL(url);
+    URLConnection c = page.openConnection();
+    BufferedReader in = new BufferedReader(new InputStreamReader(c.getInputStream()));
 
-    // This is the function used in the JSTL extended lang
-    // Returns the html response for a given `url` param
-    public static String fetch(String url) throws Exception {
-        // Now's date
-        GregorianCalendar calendar = new GregorianCalendar();
-        Date now = calendar.getTime();
+    String content = "";
+    String line;
 
-        // First check the cache... does it exist? and is it recent enough?
-        UrlCacheEntry cached = urlCache.get(url);
-        if (cached != null && now.before(cached.date)) {
-           return cached.value;
-        }
+    while ((line = in.readLine()) != null)
+      content += line;
+    in.close();
 
-        // Go fetch the URL
-        URL page = new URL(url);
-        URLConnection c = page.openConnection();
-        BufferedReader in = new BufferedReader(new InputStreamReader(c.getInputStream()));
+    // Tommorrow's date
+    calendar.add(calendar.DAY_OF_MONTH, 1);
+    Date tomorrow = calendar.getTime();
 
-        String content = "";
-        String line;
+    // Saving cache entry for URL Fetch
+    UrlCacheEntry entry = new UrlCacheEntry();
+    entry.date = tomorrow;
+    entry.value = content;
+    urlCache.put(url, entry);
 
-        while ((line = in.readLine()) != null) 
-            content += line;
-        in.close();
-
-        // Tommorrow's date
-        calendar.add(calendar.DAY_OF_MONTH, 1);
-        Date tomorrow = calendar.getTime();
-
-        // Saving cache entry for URL Fetch
-        UrlCacheEntry entry = new UrlCacheEntry();
-        entry.date = tomorrow;
-        entry.value = content;
-        urlCache.put(url, entry);
-
-        return content;
-    }
+    return content;
+  }
 }
